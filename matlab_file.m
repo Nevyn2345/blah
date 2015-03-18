4870ab0ede9589c84912f6d6bbb34efd104c3ec4
Sam Barnett <sbarnett1@sheffield.ac.uk>
2015-03-18 16:20:01 +0000

    working model of 2d data

--- matlab_file.m
+++ matlab_file.m
@@ -38,11 +38,36 @@ weight = MedData.Weight; % y
 % We'll use a quadratic model
 
 % step 1: plot the raw data (scatter3)
-scatter3(height, waist, weight)
+figure
+scatter3(height, waist, weight, 'kx')
 
 % step 2: Clean up the data (isnan)
-missingVals = isnan(height) | isnan(waist) | isnan(weight);
-height_c = height(~missingVals);
-waist_c = waist(~missingVals);
-weight_c = weight(~missingVals);
+badRows = any(isnan([height, waist, weight]),2);
+height_c = height(~badRows);
+waist_c = waist(~badRows);
+weight_c = weight(~badRows);
 % step 3: formulate and solve the model
+
+
+b = weight_c;
+A = [ones(size(height_c)), height_c, height_c.^2, ...
+    waist_c, waist_c.^2 waist_c.*height_c];
+
+modelCoeffs = A\b;
+
+% Step 4: Visualisation.
+modelFun = @(c, x1, x2) c(1) + c(2)*x1 + c(3)*x1.^2+ ...
+        c(4)*x2 + c(5)*x2.^2 + c(6)*x1.*x2;
+    
+% Substep 1: make vector data for x1 and x2.
+x1Vec = linspace(min(height_c), max(height_c), 150);
+x2Vec = linspace(min(waist_c), max(height_c), 150);
+% Substep 2: turn this into a grid
+[X1, X2] = meshgrid(x1Vec, x2Vec);
+% Substep 3: evaulate the model on the grid.
+modelOnGrid = modelFun(modelCoeffs, X1, X2);
+hold on
+surf(X1, X2, modelOnGrid, 'EdgeAlpha', 0);
+% EdgeAlpha - transparency of edges in the grid
+%shading interp % Use interpolated shading.
+
